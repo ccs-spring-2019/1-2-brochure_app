faf3e0aa4e37e51f5978599df3dd8768ec4751df
MC3D <m.chapman.home@gmail.com>
2019-05-19 09:42:25 -0400

    updates for Heroku deployment

--- conf/settings.py
+++ conf/settings.py
@@ -25,7 +25,7 @@ SECRET_KEY = 'w%*&@%%)=##0_4&8plqtbo#_ynh$_4ytyo&-^fjtx$jlnxm8&!'
 # SECURITY WARNING: don't run with debug turned on in production!
 DEBUG = True
 
-ALLOWED_HOSTS = []
+ALLOWED_HOSTS = ['vast-harbor-52296.herokuapp.com/']
 
 
 # Application definition
